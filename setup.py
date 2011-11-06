d67511a0a7249e3208b261e2b35d511e47d5f7e2
Dominique Guardiola <contact@quinode.fr>
2011-11-06 21:04:56 +0100

    setup.py packaging

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup, find_packages
+
+VERSION = __import__('djaloha').__version__
+
+import os
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-coop-cms',
+    version = VERSION,
+    description='Small CMS built around a tree navigation open to any django models',
+    packages=['coop_cms','coop_cms.management','coop_cms.templatetags',,'coop_cms.migrations'],
+    include_package_data=True,
+    author='Luc Jean',
+    author_email='ljean@apidev.fr',
+    license='BSD',
+    #long_description=read('README.txt'),
+    download_url='git://github.com/quinode/coop_cms.git',
+    zip_safe=False,
+)
+
