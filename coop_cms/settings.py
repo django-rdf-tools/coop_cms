8b2d861169a6175a031a6567d61e02092353813d
Luc <ljean@apidev.fr>
2012-01-12 15:54:17 +0100

    Permettre de gérer dans l'arbo des applis avec des sous-modules ex : coop_cms.apps.demo_cms

--- coop_cms/settings.py
+++ coop_cms/settings.py
@@ -9,13 +9,13 @@ from django.utils.importlib import import_module
 def get_navigable_content_types():
     ct_choices = []
     content_apps = config_value(u'coop_cms', 'CONTENT_APPS')
-    navigable_content_types = ContentType.objects.filter(app_label__in=content_apps).order_by('app_label')
+    apps_labels = [app.rsplit('.')[-1] for app in content_apps]
+    navigable_content_types = ContentType.objects.filter(app_label__in=apps_labels).order_by('app_label')
     for ct in navigable_content_types:
         is_navnode = ((ct.model == 'navnode') and (ct.app_label == 'coop_cms'))
         if (not is_navnode) and 'get_absolute_url' in dir(ct.model_class()):
             ct_choices.append((ct.id, ct.app_label+u'.'+ct.model))
     return ct_choices
-        
 
 def get_article_class():
     if hasattr(get_article_class, '_cache_class'):
