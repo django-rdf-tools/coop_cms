5ab14465c022f7603b7bf5fa03ef324d9aa65f42
Dominique Guardiola <dguardiola@quinode.fr>
2012-07-25 18:53:09 +0200

    calling an object in a template brings its ASCII str() representation, therefore it's bad

--- coop_cms/views.py
+++ coop_cms/views.py
@@ -440,9 +440,11 @@ def view_navnode(request, tree):
     #load and render template for the object
     #try to load the corresponding template and if not found use the default one
     model_name = unicode(node.content_type)
+    object_label = unicode(node.content_object)
     tplt = select_template(["coop_cms/navtree_content/{0}.html".format(node.content_type),
                             "coop_cms/navtree_content/default.html"])
-    html = tplt.render(RequestContext(request, {'node': node, "admin_url": admin_url, "model_name": model_name}))
+    html = tplt.render(RequestContext(request, {"node": node, "admin_url": admin_url,
+                                                "model_name": model_name, "object_label": object_label}))
 
     #return data has dictionnary
     response['html'] = html
